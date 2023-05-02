5dccfef22b80eed30b5fc269a6a97d378a350639
“Vasiliy <vasenka_kuznetsov_1997@mail.ru>
2023-05-02 22:34:15 +0300

    add_code

---
+++ src/test/java/ru/netology/Test/AuthorizationTest.java
@@ -0,0 +1,56 @@
+package ru.netology.Test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.Data.DataGenerator.Registration.generateRegisteredUser;
+import static ru.netology.Data.DataGenerator.Registration.generateUser;
+
+
+public class AuthorizationTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void successfulLoginRegisteredUser() {
+        var registeredUser = generateRegisteredUser("active");
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("h2").shouldHave(Condition.text("  Личный кабинет")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void getErrorIfNotRegisteredUser() {
+        var NotRegisteredUser = generateUser("active");
+        $("[data-test-id=login] input").setValue(NotRegisteredUser.getLogin());
+        $("[data-test-id=password] input").setValue(NotRegisteredUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__title").shouldHave(Condition.text("Ошибка")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void showLoggedInUserWithWrongPassword() {
+        var registeredUser = generateRegisteredUser("active");
+        var NotRegisteredPassword = generateUser("active");
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(NotRegisteredPassword.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__title").shouldHave(Condition.text("Ошибка")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    void showBlockedUser() {
+        var blockedUser = generateRegisteredUser("blocked");
+        $("[data-test-id=login] input").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] input").setValue(blockedUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(Condition.text("Ошибка! Пользователь заблокирован")).shouldBe(Condition.visible);
+    }
+}
